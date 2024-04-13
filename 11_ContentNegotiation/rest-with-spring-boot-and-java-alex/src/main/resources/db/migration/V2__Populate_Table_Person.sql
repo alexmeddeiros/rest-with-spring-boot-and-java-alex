d19a7d663dea1fc9da044e40c775edb5406d6f1c
Alex <alexmeddeiros@gmail.com>
2024-04-12 23:30:25 -0300

    ContentNegotiation

---
+++ 11_ContentNegotiation/rest-with-spring-boot-and-java-alex/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,11 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Brazil', 'ALEX MM', 'Male', 'Medeiros'),
+	(3, 'Brazil', 'ALEX MMed', 'Male', 'Medeiros'),
+	(4, 'Brazil', 'ALEX MMed', 'Male', 'Medeiros'),
+	(5, 'Brazil', 'ALEX MMed', 'Male', 'Medeiros'),
+	(6, 'Brazil', 'ALEX MMed', 'Male', 'Medeiros'),
+	(7, 'Brazil', 'ALEX MMed', 'Male', 'Medeiros'),
+	(8, 'Brazil', 'ALEX MMed', 'Male', 'Medeiros'),
+	(9, 'Brazil', 'ALEX MMed', 'Male', 'Medeiros'),
+	(10, 'Brazil', 'ALEX MMed', 'Male', 'Medeiros'),
+	(11, 'Brazil', 'ALEX MMed', 'Male', 'Medeiros');
