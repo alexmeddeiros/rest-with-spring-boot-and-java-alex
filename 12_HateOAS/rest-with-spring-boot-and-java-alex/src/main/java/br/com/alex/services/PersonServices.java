f25f1c1620e1fa7e34ff0701c1cd9ce1d71bf94a
Alex <alexmeddeiros@gmail.com>
2024-04-13 11:05:14 -0300

    HateOAS with Unit Tests with Mockito

---
+++ 12_HateOAS/rest-with-spring-boot-and-java-alex/src/main/java/br/com/alex/services/PersonServices.java
@@ -0,0 +1,78 @@
+package br.com.alex.services;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+import org.springframework.stereotype.Service;
+
+import br.com.alex.controllers.PersonController;
+import br.com.alex.data.vo.v1.PersonVO;
+import br.com.alex.exceptions.ResourceNotFoundException;
+import br.com.alex.mapper.DozerMapper;
+import br.com.alex.model.Person;
+import br.com.alex.repositories.PersonRepository;
+
+@Service
+public class PersonServices {
+	private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+	@Autowired
+	private PersonRepository repository;
+
+	public List<PersonVO> findAll() {
+		logger.info("Find all people");
+
+		var persons = DozerMapper.parseListObject(repository.findAll(), PersonVO.class);
+		persons
+			.stream()
+			.forEach(p -> p.add(linkTo(methodOn(PersonController.class).findById(p.getKey())).withSelfRel()));
+		return persons;
+
+	}
+
+	public PersonVO create(PersonVO person) {
+		logger.info("Creating one person");
+		var entity = DozerMapper.parseObject(person, Person.class) ;
+		PersonVO vo = DozerMapper.parseObject(entity, PersonVO.class);
+		vo.add(linkTo(methodOn(PersonController.class).findById(vo.getKey())).withSelfRel());
+		return vo;
+	}
+
+	public PersonVO update(PersonVO person) {
+		logger.info("Updating one person");
+		var entity = repository.findById(person.getKey())
+				.orElseThrow(() -> new ResourceNotFoundException("No Records nor Fond for this ID"));
+
+		entity.setAddress(person.getAddress());
+		entity.setFirstName(person.getFirstName());
+		entity.setLastName(person.getLastName());
+		entity.setGender(person.getGender());
+		PersonVO vo = DozerMapper.parseObject(entity, PersonVO.class);
+		vo.add(linkTo(methodOn(PersonController.class).findById(vo.getKey())).withSelfRel());
+		return vo;
+	}
+
+	public void delete(Long id) {
+		logger.info("Deleting one person");
+
+		var entity = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No Records nor Fond for this ID"));
+
+		repository.delete(entity);
+	}	
+
+	public PersonVO findById(Long id) {
+
+		logger.info("Find one PersonVO");
+		var entity =  repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No Records nor Fond for this ID"));
+		
+		PersonVO vo = DozerMapper.parseObject(entity, PersonVO.class);
+		vo.add(linkTo(methodOn(PersonController.class).findById(vo.getKey())).withSelfRel());
+		return vo;
+	}
+
+}
---
+++ 12_HateOAS/rest-with-spring-boot-and-java-alex/src/test/java/br/com/alex/unittest/mockito/services/PersonServiceTest.java
@@ -0,0 +1,82 @@
+package br.com.alex.unittest.mockito.services;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import br.com.alex.model.Person;
+import br.com.alex.repositories.PersonRepository;
+import br.com.alex.services.PersonServices;
+import br.com.alex.unittest.mapper.mocks.MockPerson;
+
+@TestInstance(Lifecycle.PER_CLASS)
+@ExtendWith(MockitoExtension.class)
+class PersonServiceTest {
+
+	MockPerson input;
+	
+	@InjectMocks
+	private PersonServices serivce;
+	
+	@Mock
+	PersonRepository repository;
+	
+	@BeforeEach
+	void setUpMocks() throws Exception {
+		input = new MockPerson();
+		MockitoAnnotations.openMocks(this);
+	}
+
+	@Test
+	void testFindAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCreate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDelete() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindById() {
+		Person person = input.mockEntity();
+		person.setId(1L);
+		
+		when(repository.findById(1L)).thenReturn(Optional.of(person));
+		var result =  serivce.findById(1L);
+		assertNotNull(result);
+		assertNotNull(result.getKey());
+		assertNotNull(result.getLinks());
+		System.out.println(result.toString());
+		assertTrue(result.toString().contains("links: [</api/person/v1/1>;rel=\"self\"]"));
+		assertEquals("Address Test1", result.getAddress());
+		assertEquals("First Name Test1", result.getFirstName());
+		assertEquals("Last Name Test1", result.getLastName());
+		assertEquals("Female", result.getGender());
+	}
+
+}
