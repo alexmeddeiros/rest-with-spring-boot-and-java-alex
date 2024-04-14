7d5057ff79298da564770e11f6849c25f063c3cc
Alex <alexmeddeiros@gmail.com>
2024-04-14 11:29:31 -0300

    Open API with annotation operation and custom OpenAPI

---
+++ 13_Swagger/rest-with-spring-boot-and-java-alex/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,11 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Brazil', 'ALEX MM', 'Male', 'Medeiros'),
+	(3, 'Brazil', 'Sabrina', 'Famale', 'Medeiros'),
+	(4, 'Brazil', 'Anthony', 'Male', 'Medeiros'),
+	(5, 'Brazil', 'Joaquim', 'Male', 'Figueredo'),
+	(6, 'Brazil', 'Marcos', 'Male', 'Medeiros'),
+	(7, 'Brazil', 'Maria', 'Famale', 'Stamps'),
+	(8, 'Brazil', 'Joana', 'Famale', 'Silva'),
+	(9, 'Brazil', 'Karen', 'Famale', 'Medeiros'),
+	(10, 'Brazil', 'Silvia', 'Famale', 'Aleijo'),
+	(11, 'Brazil', 'Leticia', 'Famale', 'Bim');
